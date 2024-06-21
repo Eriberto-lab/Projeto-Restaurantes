307f44e7317d289c618b460ce62415be815108d5
Eriberto Lima <eribertosflima@gmail.com>
2024-06-21 09:20:35 -0300

    Feat: criação do contextfactory para criação do contexto em tempo de desing

---
+++ Restaurantes/Restaurantes.Infrastructure/Factories/AppDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Restaurantes.Infrastructure.Context;
+using System.IO;
+
+namespace Restaurantes.Infrastructure.Factories
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var basePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\Restaurantes.API"));
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
