13d40c647df26cace3b0ef8755a72a4c52513a82
Eriberto Lima <eribertosflima@gmail.com>
2024-06-28 09:11:58 -0300

    feat: Cria interface para service de usuario

---
+++ Restaurantes/Restaurantes.Application/Interfaces/IUsuarioService.cs
@@ -0,0 +1,20 @@
+﻿using Restaurantes.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurantes.Application.Interfaces
+{
+    public interface IUsuarioService
+    {
+        Task<IEnumerable<UsuarioDTO>> GetAll();
+
+        Task<UsuarioDTO> GetById(int id);
+        Task Add(UsuarioDTO usuarioDTO);
+
+        Task Update(UsuarioDTO usuarioDTO);
+        Task Delete(int? id);
+    }
+}
