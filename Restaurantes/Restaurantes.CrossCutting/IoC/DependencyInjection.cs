c614008e071017360455c97f983e67d83b782ee3
Eriberto Lima <eribertosflima@gmail.com>
2024-06-28 09:14:08 -0300

    feat: configura injeção de dependencia

---
+++ Restaurantes/Restaurantes.CrossCutting/IoC/DependencyInjection.cs
@@ -0,0 +1,61 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using Restaurantes.Application.Interfaces;
+//using Restaurantes.Application.Mappings;
+//using Restaurantes.Application.Services;
+//using Restaurantes.Domain.Interfaces;
+//using Restaurantes.Infrastructure.Context;
+//using Restaurantes.Infrastructure.Repositories;
+
+//namespace Restaurantes.CrossCutting.IoC
+//{
+//    public static class DependencyInjectionAPI
+//    {
+//        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services,
+//        IConfiguration configuration)
+//        {
+//            services.AddDbContext<AppDbContext>(options =>
+//               options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")
+//                   ));
+
+//            services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+//            services.AddScoped<IUsuarioService, UsuarioService>();
+
+//            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+//            return services;
+//        }
+//    }
+//}
+
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurantes.Application.Interfaces;
+using Restaurantes.Application.Mappings;
+using Restaurantes.Application.Services;
+using Restaurantes.Domain.Interfaces;
+using Restaurantes.Infrastructure.Context;
+using Restaurantes.Infrastructure.Repositories;
+
+namespace Restaurantes.CrossCutting.IoC
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services,
+        IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+               options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+            services.AddScoped<IUsuarioService, UsuarioService>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            return services;
+        }
+    }
+}
