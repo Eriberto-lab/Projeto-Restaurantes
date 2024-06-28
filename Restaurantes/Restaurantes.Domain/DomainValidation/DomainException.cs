2e937575070361a911ffc56a3a780fb866f9df53
Eriberto Lima <eribertosflima@gmail.com>
2024-06-28 09:15:02 -0300

    feat: cria classe para gerenciar exceção de dominio

---
+++ Restaurantes/Restaurantes.Domain/DomainValidation/DomainException.cs
@@ -0,0 +1,9 @@
+﻿namespace Restaurantes.Domain.DomainValidation
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
