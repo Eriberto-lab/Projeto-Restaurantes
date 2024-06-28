df13d315ad00e3e61aada6a93e77317b0f752f89
Eriberto Lima <eribertosflima@gmail.com>
2024-06-28 09:10:57 -0300

    feat: Inicia criação do controller de usuarios

---
+++ Restaurantes/Restaurantes.API/Controllers/UsuariosController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurantes.Application.DTOs;
+using Restaurantes.Application.Interfaces;
+using Restaurantes.Domain.Entities;
+
+namespace Restaurantes.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public UsuariosController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UsuarioDTO>>> Get()
+        {
+            try
+            {
+                var usuarios = await _usuarioService.GetAll();
+                return Ok(usuarios);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        [HttpGet("{id}", Name = "GetUsuario")]
+        public async Task<ActionResult<Usuario>> Get(int id)
+        {
+            var usuario = await _usuarioService.GetById(id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] UsuarioDTO usuarioDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _usuarioService.Add(usuarioDto);
+
+            return new CreatedAtRouteResult("GetUsuario",
+                new { id = usuarioDto.Id }, usuarioDto);
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] UsuarioDTO usuarioDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (id != usuarioDto.Id)
+            {
+                return BadRequest();
+            }
+            await _usuarioService.Update(usuarioDto);
+            return Ok(usuarioDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Usuario>> Delete(int id)
+        {
+            var usuarioDTO = await _usuarioService.GetById(id);
+            if (usuarioDTO == null)
+            {
+                return NotFound();
+            }
+            await _usuarioService.Delete(id);
+            return Ok(usuarioDTO);
+        }
+    }
+}
