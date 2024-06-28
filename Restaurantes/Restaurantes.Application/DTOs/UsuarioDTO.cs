a44ce88bf1fa521f2544e8b3d1efc93b6865bba2
Eriberto Lima <eribertosflima@gmail.com>
2024-06-28 09:11:19 -0300

    feat: Cria DTO para usuario

---
+++ Restaurantes/Restaurantes.Application/DTOs/UsuarioDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurantes.Application.DTOs
+{
+    public class UsuarioDTO
+    {
+        [Required]
+        
+        public int Id {  get; set; }
+
+        [MaxLength(100)]
+        [MinLength(3)]
+        
+        public string? Nome { get;  set; }
+
+        [MaxLength(300)]
+       
+        public string? EnderecoDaEmpresa { get;  set; }
+
+        public decimal PrecoMedioPorRefeicao { get;  set; }
+
+        public int DiasPresenciais { get;  set; }
+
+        public decimal ValorDoValeRefeicao { get; set; }
+    }
+}
