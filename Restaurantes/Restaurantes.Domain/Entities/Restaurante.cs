14eef0e36c4a093132b9770cdc92da25b2cbeed0
Eriberto Lima <eribertosflima@gmail.com>
2024-07-01 14:28:14 -0300

    feat: adiciona modelo rico para entidade restaurante

--- Restaurantes/Restaurantes.Domain/Entities/Restaurante.cs
+++ Restaurantes/Restaurantes.Domain/Entities/Restaurante.cs
@@ -8,5 +8,29 @@ namespace Restaurantes.Domain.Entities
 {
     public sealed class Restaurante : BaseEntity
     {
+        public string Nome { get; private set; }
+        public string EnderecoRestaurante { get; private set; }
+        public decimal PrecoMedio { get; private set; }
+        public bool IsBlocked { get;  private set; }
+        public IEnumerable<Avaliacao> Avaliacaos { get; private set; }
+
+        public Restaurante(string nome, string endereco, decimal precoMedio)
+        {
+            Nome = nome;
+            EnderecoRestaurante = endereco;
+            PrecoMedio = precoMedio;
+
+        }
+
+        public void BlockRestaurant()
+        {
+            IsBlocked = true;
+        }
+
+        public void UnblockRestaurant()
+        {
+            IsBlocked = false;
+        }
+
     }
 }
