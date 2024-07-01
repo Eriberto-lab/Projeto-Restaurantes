750c22591a70e2aabfd12dc394d7de6f95fca3f5
Eriberto Lima <eribertosflima@gmail.com>
2024-07-01 16:01:23 -0300

    feat: iniciar criação do controller de Restaurantes com a rota getAll

---
+++ Restaurantes/Restaurantes.API/Controllers/RestaurantesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Restaurantes.Application.DTOs;
+using Restaurantes.Application.Interfaces;
+
+namespace Restaurantes.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantesController : ControllerBase
+    {
+        private readonly IRestauranteService _restauranteService;
+
+        public RestaurantesController(IRestauranteService restauranteService)
+        {
+            _restauranteService = restauranteService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<RestauranteDTO>> Get()
+        {
+            var restaurantes = await _restauranteService.GetAll();
+
+            Ok(restaurantes);
+            return restaurantes;
+        }
+    }
+}
