ffad19662bd72207a1589f7ba33f189470804921
Eriberto Lima <eribertosflima@gmail.com>
2024-07-01 15:57:19 -0300

    feat: cria repositorio para a Entidade Restaurante

---
+++ Restaurantes/Restaurantes.Infrastructure/Repositories/RestauranteRepository.cs
@@ -0,0 +1,18 @@
+﻿using Restaurantes.Domain.Entities;
+using Restaurantes.Domain.Interfaces;
+using Restaurantes.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurantes.Infrastructure.Repositories
+{
+    public class RestauranteRepository : BaseRepository<Restaurante>, IRestauranteRepository
+    {
+        public RestauranteRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
