251fb94c3d6007602640831d4b1fca860e43c16e
Eriberto Lima <eribertosflima@gmail.com>
2024-07-01 16:00:44 -0300

    feat: cria migration para implementação da entidade Restaurante no DB

---
+++ Restaurantes/Restaurantes.Infrastructure/Migrations/20240701184447_Restaurante.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurantes.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Restaurante : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Restaurantes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EnderecoRestaurante = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PrecoMedio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IsBlocked = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurantes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Avaliacao",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RestauranteId = table.Column<int>(type: "int", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Avaliacao", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Avaliacao_Restaurantes_RestauranteId",
+                        column: x => x.RestauranteId,
+                        principalTable: "Restaurantes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Usuarios",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 7, 1, 15, 44, 46, 738, DateTimeKind.Local).AddTicks(3549));
+
+            migrationBuilder.UpdateData(
+                table: "Usuarios",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 7, 1, 15, 44, 46, 738, DateTimeKind.Local).AddTicks(3579));
+
+            migrationBuilder.UpdateData(
+                table: "Usuarios",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2024, 7, 1, 15, 44, 46, 738, DateTimeKind.Local).AddTicks(3581));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Avaliacao_RestauranteId",
+                table: "Avaliacao",
+                column: "RestauranteId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Avaliacao");
+
+            migrationBuilder.DropTable(
+                name: "Restaurantes");
+
+            migrationBuilder.UpdateData(
+                table: "Usuarios",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 6, 19, 13, 50, 38, 334, DateTimeKind.Local).AddTicks(3528));
+
+            migrationBuilder.UpdateData(
+                table: "Usuarios",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 6, 19, 13, 50, 38, 334, DateTimeKind.Local).AddTicks(3552));
+
+            migrationBuilder.UpdateData(
+                table: "Usuarios",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2024, 6, 19, 13, 50, 38, 334, DateTimeKind.Local).AddTicks(3554));
+        }
+    }
+}
