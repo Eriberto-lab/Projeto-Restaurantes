27ededfe50acd808351ef5ade448641fb128b00e
Eriberto Lima <eribertosflima@gmail.com>
2024-07-01 14:53:16 -0300

    feat: adiciona configuração para entidade Restaurante

---
+++ Restaurantes/Restaurantes.Infrastructure/EntitiesConfiguration/RestauranteConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Restaurantes.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurantes.Infrastructure.EntitiesConfiguration
+{
+    internal class RestauranteConfiguration : IEntityTypeConfiguration<Restaurante>
+    {
+        public void Configure(EntityTypeBuilder<Restaurante> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(p  => p.EnderecoRestaurante).IsRequired();
+            builder.Property(p => p.Nome).IsRequired();
+
+        }
+    }
+}
