0c52686d8a2acb7c00e6586598dfcb22878aca62
Eriberto Lima <eribertosflima@gmail.com>
2024-07-01 16:00:14 -0300

    feat: limpa comentarios e configura injeção de dependencia para os serviços e repositorio de restaurante.

--- Restaurantes/Restaurantes.CrossCutting/IoC/DependencyInjection.cs
+++ Restaurantes/Restaurantes.CrossCutting/IoC/DependencyInjection.cs
@@ -1,36 +1,4 @@
-﻿//using Microsoft.EntityFrameworkCore;
-//using Microsoft.Extensions.Configuration;
-//using Microsoft.Extensions.DependencyInjection;
-//using Restaurantes.Application.Interfaces;
-//using Restaurantes.Application.Mappings;
-//using Restaurantes.Application.Services;
-//using Restaurantes.Domain.Interfaces;
-//using Restaurantes.Infrastructure.Context;
-//using Restaurantes.Infrastructure.Repositories;
-
-//namespace Restaurantes.CrossCutting.IoC
-//{
-//    public static class DependencyInjectionAPI
-//    {
-//        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services,
-//        IConfiguration configuration)
-//        {
-//            services.AddDbContext<AppDbContext>(options =>
-//               options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")
-//                   ));
-
-//            services.AddScoped<IUsuarioRepository, UsuarioRepository>();
-//            services.AddScoped<IUsuarioService, UsuarioService>();
-
-//            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
-
-//            return services;
-//        }
-//    }
-//}
-
-
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Restaurantes.Application.Interfaces;
@@ -52,6 +20,9 @@ namespace Restaurantes.CrossCutting.IoC
 
             services.AddScoped<IUsuarioRepository, UsuarioRepository>();
             services.AddScoped<IUsuarioService, UsuarioService>();
+
+            services.AddScoped<IRestauranteRepository, RestauranteRepository>();
+            services.AddScoped<IRestauranteService, RestauranteService>();
 
             services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
 
