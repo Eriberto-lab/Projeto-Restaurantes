ee1ac0c3e5754aea4fe194ad16d951b5d4aebb0c
Eriberto Lima <eribertosflima@gmail.com>
2024-07-02 11:00:13 -0300

    feat: finaliza controller inicial da entidade Restaurantes

--- Restaurantes/Restaurantes.API/Controllers/RestaurantesController.cs
+++ Restaurantes/Restaurantes.API/Controllers/RestaurantesController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Restaurantes.Application.DTOs;
 using Restaurantes.Application.Interfaces;
+using Restaurantes.Application.Services;
+using Restaurantes.Domain.Entities;
 
 namespace Restaurantes.API.Controllers
 {
@@ -24,5 +26,61 @@ namespace Restaurantes.API.Controllers
             Ok(restaurantes);
             return restaurantes;
         }
+
+        [HttpGet("{id}", Name = "GetRestaurante")]
+        public async Task<ActionResult<RestauranteDTO>> Get(int id)
+        {
+            var restaurante = await _restauranteService.GetById(id);
+
+            if (restaurante == null)
+            {
+                return NotFound();
+            }
+            return Ok(restaurante);
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult> Post(RestauranteDTO restauranteDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _restauranteService.Add(restauranteDTO);
+
+            return new CreatedAtRouteResult("GetRestaurante",
+                new { id = restauranteDTO.Id }, restauranteDTO);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, RestauranteDTO restauranteDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (id != restauranteDTO.Id)
+            {
+                return BadRequest();
+            }
+            await _restauranteService.Update(restauranteDTO);
+            return Ok(restauranteDTO);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<RestauranteDTO>> Delete(int id)
+        {
+            var usuarioDTO = await _restauranteService.GetById(id);
+            if (usuarioDTO == null)
+            {
+                return NotFound();
+            }
+            await _restauranteService.Delete(id);
+            return Ok(usuarioDTO);
+        }
+
     }
 }
